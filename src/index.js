31b77eaeeff98f70de9db39dff6dde837ad43af1
Dave Staab <dave.staab@cadre5.com>
2017-04-02 14:01:09 -0400

    lesson 2 finished

--- src/index.js
+++ src/index.js
@@ -23,3 +23,34 @@ const earth = {
         color: '#ffff99'
     }
 }
+
+
+const size = 200;
+const scale = scaleLinear()
+    .domain([0, 4025]) // miles
+    .range([0, size]); // pixels
+
+console.log('4000 to pixels is', scale(4000));
+console.log('0 to pixels is', scale(0));
+console.log('earths crust in pixels', scale(earth.crust.radius));
+
+var earthCrust = select('.earth')
+  .append('circle');
+
+earthCrust.attr('r', scale(earth.crust.radius));
+earthCrust.attr('fill', earth.crust.color);
+//
+select('.earth')
+  .append('circle')
+  .attr('r', scale(earth.mantle.radius))
+  .attr('fill', earth.mantle.color);
+
+select('.earth')
+    .append('circle')
+    .attr('r', scale(earth.outerCore.radius))
+    .attr('fill', earth.outerCore.color);
+//
+select('.earth')
+    .append('circle')
+    .attr('r', scale(earth.innerCore.radius))
+    .attr('fill', earth.innerCore.color);
