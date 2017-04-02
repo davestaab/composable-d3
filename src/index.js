734467abc621a2dbb5c98cd2eaa7233a713b3526
Dave Staab <dave.staab@cadre5.com>
2017-04-02 16:56:26 -0400

    estup before starting lesson 4

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import { scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
-
-const earth = {
-    layers: [
-        {
-            name: 'crust',
-            radius: 4025,
-            color: '#7fc97f'
-        },
-        {
-            name: 'mantle',
-            radius: 4000,
-            color: '#bf5b17'
-        },
-        {
-            name: 'outerCore',
-            radius: 2200,
-            color: '#fdc086'
-        },
-        {
-            name: 'innerCore',
-            radius: 800,
-            color: '#ffff99'
-        }
-    ]
-}
+import { earth } from './planets';
 
 const size = 200;
 const scale = scaleLinear()
---
+++ src/planets.js
@@ -0,0 +1,24 @@
+export const earth = {
+    layers: [
+        {
+            name: 'crust',
+            radius: 4025,
+            color: '#7fc97f'
+        },
+        {
+            name: 'mantle',
+            radius: 4000,
+            color: '#bf5b17'
+        },
+        {
+            name: 'outerCore',
+            radius: 2200,
+            color: '#fdc086'
+        },
+        {
+            name: 'innerCore',
+            radius: 800,
+            color: '#ffff99'
+        }
+    ]
+}
