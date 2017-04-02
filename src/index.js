2bd25c40712a8e6c95159a72235605002f1e1515
Dave Staab <dave.staab@cadre5.com>
2017-04-02 18:03:37 -0400

    lesson 4 finished

--- src/index.js
+++ src/index.js
@@ -2,17 +2,39 @@ import { scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
 import { earth } from './planets';
 
-const size = 200;
-const scale = scaleLinear()
-    .domain([0, 4025]) // miles
-    .range([0, size]); // pixels
+const planet = planetChart()
 
-const selection = select('.earth')
+const selection = select('#chart')
+    .append('svg')
+    .attr('class', 'space')
+    .attr('width', 500)
+    .attr('height', 500)
+    .append('g')
+    .attr('transform', 'translate(250, 250)')
     .selectAll('.layer')
-    .data(earth.layers);
+    .data(earth.layers)
+    .call(planet)
 
-selection.enter()
-    .append('circle')
-    .attr('class', 'layer')
-    .attr('r', d => scale(d.radius))
-    .attr('fill', d => d.color);
+function planetChart() {
+    let size = 200;
+    const scale = scaleLinear()
+        .domain([0, 4025]) // miles
+        .range([0, size]); // pixels
+
+    function chart(selection) {
+        scale.range([0, size]);
+        selection.enter()
+            .append('circle')
+            .attr('class', 'layer')
+            .attr('r', d => scale(d.radius))
+            .attr('fill', d => d.color);
+    };
+
+    chart.size = function (_) {
+        if (!arguments.length) return size;
+        size = _;
+        return chart;
+    };
+
+    return chart;
+}
