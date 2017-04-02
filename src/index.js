a956d3ebecc3750465b83b54ee5cb14bae8ff679
Dave Staab <dave.staab@cadre5.com>
2017-04-02 15:25:44 -0400

    setting up the start of lesson 3

--- src/index.js
+++ src/index.js
@@ -2,26 +2,28 @@ import { scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
 
 const earth = {
-    crust: {
-        name: 'crust',
-        radius: 4025,
-        color: '#7fc97f'
-    },
-    mantle: {
-        name: 'mantle',
-        radius: 4000,
-        color: '#bf5b17'
-    },
-    outerCore: {
-        name: 'outerCore',
-        radius: 2200,
-        color: '#fdc086'
-    },
-    innerCore: {
-        name: 'innerCore',
-        radius: 800,
-        color: '#ffff99'
-    }
+    layers: [
+        {
+            name: 'crust',
+            radius: 4025,
+            color: '#7fc97f'
+        },
+        {
+            name: 'mantle',
+            radius: 4000,
+            color: '#bf5b17'
+        },
+        {
+            name: 'outerCore',
+            radius: 2200,
+            color: '#fdc086'
+        },
+        {
+            name: 'innerCore',
+            radius: 800,
+            color: '#ffff99'
+        }
+    ]
 }
 
 
